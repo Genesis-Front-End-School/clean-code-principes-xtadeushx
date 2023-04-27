14382429b9aa6c4ea18da5078b1d545fd937be71
xtadeushx <alekscherkasov13@gmail.com>
2023-04-27 13:34:51 +0300

    fix: add right types for  pagination component

--- src/components/paginate/pagination.tsx
+++ src/components/paginate/pagination.tsx
@@ -2,13 +2,15 @@ import ReactPaginate from 'react-paginate';
 import { useState } from 'hooks/hooks';
 import { CoursesLayout } from 'components/courses/courses-layout';
 import { Course } from 'common/types/coursesList.types';
-import styles from './pagination.module.scss';
 import Spinner from 'components/common/loader/loader';
+
+import styles from './pagination.module.scss';
+
 interface IPaginatedItemsProps {
   itemsPerPage: number;
   courses: Course[];
   loading: 'idle' | 'pending' | 'succeeded' | 'failed';
-  error: any;
+  error: Error | null;
 }
 
 const PaginatedCourses: React.FC<IPaginatedItemsProps> = ({
@@ -17,22 +19,24 @@ const PaginatedCourses: React.FC<IPaginatedItemsProps> = ({
   loading,
   error,
 }) => {
-  const [itemOffset, setItemOffset] = useState(0);
+  const [itemOffset, setItemOffset] = useState<number>(0);
 
   const endOffset = itemOffset + itemsPerPage;
   const currentItems = courses.slice(itemOffset, endOffset);
   const pageCount = Math.ceil(courses.length / itemsPerPage);
 
-  const handlePageClick = (event: any) => {
-    const newOffset = (event.selected * itemsPerPage) % courses.length;
+  const handlePageClick = (selectedItem: { selected: number }) => {
+    const newOffset = (selectedItem.selected * itemsPerPage) % courses.length;
     setItemOffset(newOffset);
   };
+
+
   if (loading === 'pending') {
-    return <Spinner isOverflowParent />
-  };
+    return <Spinner isOverflowParent />;
+  }
   if (error) {
-    return <h3>{`Server response with  ${error.toString()}`}</h3>
-  };
+    return <h3>{`Server response with ${error.toString()}`}</h3>;
+  }
 
   return (
     <>
@@ -54,3 +58,4 @@ const PaginatedCourses: React.FC<IPaginatedItemsProps> = ({
   );
 };
 export { PaginatedCourses };
+

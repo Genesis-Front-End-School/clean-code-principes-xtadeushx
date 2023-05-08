206172b2d6c93080c610c662a6cab7b6859fcac2
xtadeushx <alekscherkasov13@gmail.com>
2023-05-08 20:40:42 +0300

    fix: change test for course-details remove comments

--- src/components/courses/components/course-details/course-details.test.tsx
+++ src/components/courses/components/course-details/course-details.test.tsx
@@ -19,36 +19,4 @@ describe('CourseDetails', () => {
     render(<CourseDetails />);
     expect(screen.getByText('Loading...')).toBeInTheDocument();
   });
-
-  // it('renders error message on error', () => {
-  //   (useParams as jest.Mock).mockReturnValue({ id: '1' });
-  //   (useFetch as jest.Mock).mockReturnValue({
-  //     loading: 'failed',
-  //     error: new Error('Failed to fetch'),
-  //   });
-
-  //   render(<CourseDetails />);
-  //   expect(
-  //     screen.getByText('Server response with  Error: Failed to fetch')
-  //   ).toBeInTheDocument();
-  // });
-
-  // it('renders course info on success', () => {
-  //   const course = {
-  //     id: '1',
-  //     title: 'Test Course',
-  //     description: 'This is a test course',
-  //     instructor: 'John Doe',
-  //     duration: 10,
-  //     publishDate: new Date(),
-  //   };
-  //   (useParams as jest.Mock).mockReturnValue({ id: '1' });
-  //   (useFetch as jest.Mock).mockReturnValue({
-  //     loading: 'succeeded',
-  //     response: course,
-  //   });
-
-  //   render(<CourseDetails key={33} />);
-  //   expect(screen.getByText('Test Course')).toBeInTheDocument();
-  // });
 });
